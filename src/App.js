df8ba381ff28480f590b4ddb9931b1f1b0dc3c42
서유나 <ckrgody40@naver.com>
2023-07-20 09:58:14 +0900

    react

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = '수영 맛집'; 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>BLOG</h4>
+      </div>
+      <h4 style={ {color : 'red' , fontSize : '16px'} }>{ post }</h4>
     </div>
   );
 }
