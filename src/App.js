2e02241660a9255f752ed6ad3224f2c9b8c23453
서유나 <ckrgody40@naver.com>
2023-07-23 20:39:08 +0900

    props

--- src/App.js
+++ src/App.js
@@ -11,9 +11,21 @@ function App() {
   // state변수는 html이 자동 재렌더링 됨
   // 바로바로 변경되야할경우 state에다가 
 
+  // 반복문 230723
+  [1,2,3].map(function(a){
+    console.log(a)
+  })
 
   //state는 등호로 변경하면안됨
-  let [like, likeUpdate] = useState(0);
+  let [like, likeUpdate] = useState([0,0,0]);
+
+
+  //모달
+  let [modal, setModal] = useState(false);
+
+  //tit
+  
+  let [title, setTit] = useState(1);
 
   return (
     <div className="App">
@@ -21,6 +33,7 @@ function App() {
         <h4>ReactBLOG</h4>
       </div>
       {/* <h4 style={ {color : 'red' , fontSize : '16px'} }>{ post }</h4> */}
+
 
       <button onClick={ () => { 
           // array/object 다룰때 원본은 보존하는게 좋음
@@ -30,6 +43,7 @@ function App() {
             copy[0] = '여자코트 추천';
             글제목변경(copy) 
           }}>글수정하기</button>
+
         <button onClick={ () => { 
           let copy = [...글제목];
           copy.sort();
@@ -38,35 +52,81 @@ function App() {
         }}>가나다순정렬</button>
 
 
-        <div className="list">
+        {/* <div className="list"> */}
        
 
           {/* onclick={} 안엔 함수 이름을 넣어야함 함수정의부분을 넣어도되고 위에다가 정의해도됨*/}
-         <h4>{ 글제목[0] }<span onClick={ () => { likeUpdate(like+1)  }}> 🧡 </span> { like } </h4>
+         {/* <h4>{ 글제목[0] }<span onClick={ () => { likeUpdate(like+1)  }}> 🧡 </span> { like } </h4>
          <p>7월 2
           1일</p>
         </div>
+
         <div className="list">
          <h4>{ 글제목[1] }</h4>
          <p>7월 2
           1일</p>
         </div>
+
+
+
         <div className="list">
-         <h4>{ 글제목[2] }</h4>
+         <h4 onClick={ () => { setModal(true) } }>{ 글제목[2] }</h4>
          <p>7월 2
           1일</p>
-        </div>
+        </div> */}
+
+
+        {/* 
         
+        동적 UI 만들기 STEP
+         1. html  css 로 미리 디자인 완성
+         2.  UI의 현재상태를 state 저장
+         3. state 따라 UI 가 어떻게 보일지 설정
         
+        */}
+        
+        {/* 반복문 */}
+        {
+          //실제 글 갯수만큼 
+          글제목.map(function(a, i){
+            return (
+              <div className="list" key={i}>
+                <h4 onClick ={ () => { setModal(!modal); setTit(i)}}> { 글제목[i] }</h4>
+                <span onClick={ () => {
+                    let copy =[...like];
+                    copy[i] = copy[i] + 1;
+                    likeUpdate(copy)  
+                   }}> 🧡 </span>{ like[i] }
+                <p>7월 21일</p>
+              </div>
+             
+
+            )
+          })
+
+        }
+
+        
+        {/* 
+        <button onClick={()=>{setTit(0)}}>글제목0</button>
+        <button onClick={()=>{setTit(1)}}>글제목1</button>
+        <button onClick={()=>{setTit(2)}}>글제목2</button>
+         */}
+
+
         {/* 컴포넌트 만들기 1.function 2. return() 안에 담기 3<함수명></함수명> */}
-        <Modal/>
+       
+
+        {/* {} 안에는 if for같은 조건문 사용못함 if 대용의 삼항연산자사용
+           조건식 ? 참일떄코드 : 거짓일때코드
         
-        {/* {글제목.map((title, index) => (
-          <div className="list">
-          <h4 key={index}>{title}</h4>
-          <p>7월 21일</p>
-          </div>
-      ))} */}
+        */}
+        {
+          modal == true ? <Modal 글제목변경={글제목변경} color={'skyblue'} 글제목={글제목} title={title}/> : '' 
+         
+        }
+
+       
     </div>
   );
 }
@@ -77,14 +137,29 @@ function App() {
   
 // }
 
-function Modal(){
+function Modal(props){
   return (
     // 의미없는<div>대신 <></> 사용
     <> 
-      <div className="modal">
-        <h4>제목</h4>
+      <div className="modal" style={ {background: props.color} }>
+        {/* 
+        
+        누른 리스트의 제목가져오기!!  props 사용
+        
+        
+        */}
+        <h4>{props.글제목[props.title]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
+        <button onClick={ () => { 
+        
+          let copy = [...props.tit];
+          copy[0] = '여자코트 추천';
+          props.글제목변경(copy) 
+
+         }}>글수정하기</button>
+
+        
       </div>
     </>
   )
